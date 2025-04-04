e3ecdfbaa98ec5600f784989d614d5df02754eeb
Derek Alves <dev.derek.alves@gmail.com>
2025-04-04 17:23:22 -0300

    create view model for flutter activity

--- main_app/app/src/main/java/com/composablecode/main_app/app/activity/flutter/MainFlutterActivity.kt
+++ main_app/app/src/main/java/com/composablecode/main_app/app/activity/flutter/MainFlutterActivity.kt
@@ -2,7 +2,6 @@ package com.composablecode.main_app.app.activity.flutter
 
 import android.content.Context
 import android.content.Intent
-import com.composablecode.main_app.domain.model.FeatureModel
 import com.composablecode.main_app.infra.flutter.flutter_engine.MainFlutterEngineHandler
 import io.flutter.embedding.android.FlutterActivity
 import org.koin.android.ext.android.inject
@@ -15,6 +14,4 @@ class MainFlutterActivity : FlutterActivity() {
             MainFlutterEngineHandler.ENGINE_ID
         ).build(context).apply { addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) }
     }
-
-   
 }
